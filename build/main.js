4042f543388d73eb5d9469c2ff849a09fa7d5af4
PavelSolovev <pashius2010@gmail.com>
2020-10-31 13:08:28 +0400

    add webpack

--- build/main.js
+++
@@ -1,6 +0,0 @@
-import React from "react";
-import ReactDOM from "react-dom";
-export const App = () => {
-  return /*#__PURE__*/React.createElement("div", null, "Hello World!");
-};
-ReactDOM.render( /*#__PURE__*/React.createElement(App, null), document.getElementById("root"));
--- src/main.js
+++
@@ -1,8 +0,0 @@
-import React from "react";
-import ReactDOM from "react-dom";
-
-export const App = () => {
-  return <div>Hello World!</div>;
-};
-
-ReactDOM.render(<App />, document.getElementById("root"));
